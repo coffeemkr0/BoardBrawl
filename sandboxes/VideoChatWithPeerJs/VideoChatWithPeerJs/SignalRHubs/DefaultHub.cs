ce2cff811a81db1c13aecf62fd89e87561030d2d
Michael Coffey <mike.e.coffey@gmail.com>
2023-04-12 20:48:49 -0500

    Add a method to the DefaultHub that a client (browser) can call to join a chat room. See an example of a Hub I made here that has public methods and also events to clients - https://github.com/coffeemkr0/Cloudflow/blob/master/Cloudflow/Cloudflow.Core/Agents/AgentHub.cs

--- sandboxes/VideoChatWithPeerJs/VideoChatWithPeerJs/SignalRHubs/DefaultHub.cs
+++ sandboxes/VideoChatWithPeerJs/VideoChatWithPeerJs/SignalRHubs/DefaultHub.cs
@@ -4,6 +4,10 @@ namespace VideoChatWithPeerJs.SignalRHubs
 {
     public class DefaultHub : Hub
     {
-
+        public async Task JoinRoom(string roomId, string userId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            await Clients.Group(roomId).SendAsync("user-connected", userId);
+        }
     }
 }
