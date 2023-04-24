c21c36ad078f17a171e06a104009efa8351c49bc
adampate <adampate@gmail.com>
2023-04-24 11:03:15 -0500

    Updated Program.cs with compiling code to detect card-shaped objects in a video stream.
    Need to be able to test though.

--- sandboxes/OpenCVTest/OpenCVTest/Program.cs
+++ sandboxes/OpenCVTest/OpenCVTest/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenCvSharp;
+
 
 namespace OpenCVTest
 {
@@ -22,5 +24,73 @@ namespace OpenCVTest
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public void DetectCardInVideo()
+        {
+            Console.WriteLine("DetectCardInVideo Method invoked.");
+
+            VideoCapture capture = new VideoCapture(0);
+            Mat frame = new Mat();
+            Mat gray = new Mat();
+            Mat corners = new Mat();
+            List<Point2f> cardCorners = new List<Point2f>();
+            List<Point> approxCorners = new List<Point>();
+
+            while (true)
+            {
+                capture.Read(frame);
+                Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+
+                Cv2.CornerHarris(gray, corners, 2, 3, 0.04);
+                Cv2.Normalize(corners, corners, 0, 255, NormTypes.MinMax);
+                Cv2.ConvertScaleAbs(corners, corners);
+
+                Cv2.FindContours(corners, out var contours, out var hierarchy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+                // Find the contour with the largest area
+                double maxArea = -1;
+                int maxContourIdx = -1;
+                for (int i = 0; i < contours.Length; i++)
+                {
+                    double area = Cv2.ContourArea(contours[i]);
+                    if (area > maxArea)
+                    {
+                        maxArea = area;
+                        maxContourIdx = i;
+                    }
+                }
+
+                // Approximate the corners of the card using the contour
+                InputArray contourInput = InputArray.Create(contours[maxContourIdx]);
+                Point[] approxCornersArray = new Point[contours.Length];
+
+                Cv2.ApproxPolyDP(contourInput, OutputArray.Create(new Mat(approxCornersArray.Length, 1, MatType.CV_32SC2)), 0.02 * Cv2.ArcLength(contours[maxContourIdx], true), true);
+
+                approxCorners = new List<Point>(approxCornersArray);
+
+
+                // Check if the approximated contour has 4 corners and is convex
+                if (approxCorners.Count() == 4 && Cv2.IsContourConvex(approxCorners))
+                {
+                    cardCorners = approxCorners.ConvertAll<Point2f>(p => new Point2f(p.X, p.Y));
+                }
+
+                // Draw the card corners on the frame
+                if (cardCorners.Count() == 4)
+                {
+                    Cv2.Polylines(frame, new Point[][] { cardCorners.ConvertAll<Point>(p => new Point((int)p.X, (int)p.Y)).ToArray() }, true, Scalar.Green, 2);
+                }
+
+                Cv2.ImShow("frame", frame);
+
+                if (Cv2.WaitKey(1) == 'q')
+                {
+                    break;
+                }
+            }
+
+            capture.Release();
+            Cv2.DestroyAllWindows();
+        }
     }
 }
