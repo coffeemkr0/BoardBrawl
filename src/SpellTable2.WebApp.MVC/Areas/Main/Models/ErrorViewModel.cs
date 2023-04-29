c746227cdaf7e1d6a72336af311204c3015ccda2
coffeemkr0 <mike.e.coffey@gmail.com>
2023-04-29 12:10:43 -0500

    Remove unused error view and model

--- src/SpellTable2.WebApp.MVC/Areas/Main/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SpellTable2.WebApp.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
