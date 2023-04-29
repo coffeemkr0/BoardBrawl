e158e46c8fa75119da9d70ee864eb54f88455253
coffeemkr0 <mike.e.coffey@gmail.com>
2023-04-29 11:23:49 -0500

    Remove most of the default stuff in the web app that was added by the project template.

--- src/SpellTable2.WebApp.MVC/Controllers/HomeController.cs
+++ src/SpellTable2.WebApp.MVC/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SpellTable2.WebApp.MVC.Models;
-using System.Diagnostics;
 
 namespace SpellTable2.WebApp.MVC.Controllers
 {
@@ -16,17 +14,6 @@ namespace SpellTable2.WebApp.MVC.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
