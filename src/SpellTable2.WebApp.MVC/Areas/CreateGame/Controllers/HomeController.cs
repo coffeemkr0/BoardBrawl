1e9b82d8d36c2561dbb2acc0ee3c166221c9aee4
coffeemkr0 <mike.e.coffey@gmail.com>
2023-04-29 12:13:36 -0500

    Setup areas for creating a game and playing a game

---
+++ src/SpellTable2.WebApp.MVC/Areas/CreateGame/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpellTable2.WebApp.MVC.Areas.CreateGame.Controllers
+{
+    [Area("CreateGame")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/SpellTable2.WebApp.MVC/Areas/Game/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpellTable2.WebApp.MVC.Areas.Game.Controllers
+{
+    [Area("Game")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
