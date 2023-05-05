ea5ef355a0c62cca6dfdffdd533f652ca7122569
Michael Coffey <mike.e.coffey@gmail.com>
2023-05-04 20:47:59 -0500

    Add Signal R to services

--- src/SpellTable2.WebApp.MVC/Program.cs
+++ src/SpellTable2.WebApp.MVC/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSignalR();
+
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
