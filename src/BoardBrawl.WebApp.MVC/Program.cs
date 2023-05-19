eab245e52a48764af3317af78658e2aa90d6d3ff
Michael Coffey <mike.e.coffey@gmail.com>
2023-05-19 14:01:13 -0500

    Add file logging

--- src/BoardBrawl.WebApp.MVC/Program.cs
+++ src/BoardBrawl.WebApp.MVC/Program.cs
@@ -8,6 +8,13 @@ using Microsoft.EntityFrameworkCore;
 using BoardBrawl.WebApp.MVC.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConsole();
+});
+
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -38,6 +45,9 @@ builder.Services.AddScoped<BoardBrawl.Repositories.Game.IRepository, BoardBrawl.
 builder.Services.AddScoped<BoardBrawl.Services.Game.IService, BoardBrawl.Services.Game.Service>();
 
 var app = builder.Build();
+
+var loggerFactory = app.Services.GetService<ILoggerFactory>();
+loggerFactory.AddFile(builder.Configuration["Logging:LogFilePath"].ToString());
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
