020e38cae5471054e10106276ec9948480ddb851
adampate <adampate@gmail.com>
2023-05-22 12:20:30 -0500

    Implemented Laplacian Card Detection PoC

--- sandboxes/OpenCVTest/OpenCVTest/OpenCVSharpTest.cs
+++ sandboxes/OpenCVTest/OpenCVTest/OpenCVSharpTest.cs
@@ -107,11 +107,7 @@ namespace CardDetectionExample
                     if (longestLine != null)
                     {
                         double angle = longestLine.Theta * 180 / Math.PI;
-
-                        // Rotate the image to align the most prominent line with the vertical axis
-                        //Mat rotationMatrix = Cv2.GetRotationMatrix2D(new Point2f(image.Width / 2f, image.Height / 2f), angle - 90, 1);
-                        //Cv2.WarpAffine(image, image, rotationMatrix, image.Size());
-
+                                                
                         using (Mat edges = new Mat())
                         {
                             Cv2.Canny(blurred, edges, 75, 125, 7, false);
@@ -161,7 +157,74 @@ namespace CardDetectionExample
         }
 
 
-        public void DetectCardShapes(string imagePath)
+        public void DetectCard_Laplacian(string imagePath)
+        {
+            // Load the image using OpenCVSharp4
+            Mat image = Cv2.ImRead(imagePath);
+
+            // Convert the image to grayscale
+            using (Mat gray = new Mat())
+            {
+                Cv2.CvtColor(image, gray, ColorConversionCodes.BGR2GRAY);
+
+                // Apply Gaussian blur to reduce noise
+                using (Mat blurred = new Mat())
+                {
+                    Cv2.GaussianBlur(gray, blurred, new Size(5, 5), 0);
+
+                    // Apply Laplacian edge detection
+                    using (Mat edges = new Mat())
+                    {
+                        Cv2.Laplacian(blurred, edges, MatType.CV_8U, ksize: 3, scale: 1, delta: 0, BorderTypes.Isolated);
+
+                        // Find contours in the image
+                        Point[][] contours;
+                        HierarchyIndex[] hierarchy;
+                        Cv2.FindContours(edges, out contours, out hierarchy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+                        // Filter contours based on area and aspect ratio
+                        const double minArea = 1000; // Minimum area of the card contour
+                        const double minAspectRatio = 0.8; // Minimum aspect ratio of the card contour
+                        const double maxAspectRatio = 1.2; // Maximum aspect ratio of the card contour
+
+                        foreach (var contour in contours)
+                        {
+                            double area = Cv2.ContourArea(contour);
+                            if (area < minArea)
+                                continue;
+
+                            Rect boundingRect = Cv2.BoundingRect(contour);
+                            double aspectRatio = (double)boundingRect.Width / boundingRect.Height;
+                            if (aspectRatio < minAspectRatio || aspectRatio > maxAspectRatio)
+                                continue;
+
+                            // Draw the card contour on the original image
+                            Cv2.DrawContours(image, new[] { contour }, 0, Scalar.Red, 2);
+
+                            // Display the cropped card image
+                            using (Mat croppedCard = new Mat(image, boundingRect))
+                            {
+                                Cv2.ImShow("Cropped Card Image", croppedCard);
+                                //Cv2.WaitKey(0);
+                            }
+
+                            // TODO: Process the cropped card image (e.g., perform recognition/classification)
+
+                            // Break after detecting the first suitable card contour
+                            break;
+                        }
+
+                        // Display the image with card contour(s)
+                        Cv2.ImShow("Card Detection Result", image);
+                        Cv2.WaitKey(0);
+                    }
+                }
+            }
+        }
+    
+
+
+    public void DetectCardShapes(string imagePath)
         {
             // Load the image using OpenCVSharp4
             using (Mat image = Cv2.ImRead(imagePath))
