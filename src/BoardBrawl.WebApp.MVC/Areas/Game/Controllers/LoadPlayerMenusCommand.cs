b1c639793c59384e0ef43d1660ca4d0fbce52e21
Michael Coffey <mike.e.coffey@gmail.com>
2023-07-08 11:51:54 -0500

    #44 standardize some text sizes

--- src/BoardBrawl.WebApp.MVC/Areas/Game/Controllers/LoadPlayerMenusCommand.cs
+++ src/BoardBrawl.WebApp.MVC/Areas/Game/Controllers/LoadPlayerMenusCommand.cs
@@ -6,7 +6,12 @@ namespace BoardBrawl.WebApp.MVC.Areas.Game.Controllers
     {
         public static void Execute(Model model)
         {
+            var myPlayer = model.PlayerBoard.Players.First(i => i.IsSelf);
 
+            foreach (var player in model.PlayerBoard.Players)
+            {
+                player.PlayerMenu.CanBeMadeGameOwner = myPlayer.IsGameOwner && !player.IsSelf;
+            }
         }
     }
 }
