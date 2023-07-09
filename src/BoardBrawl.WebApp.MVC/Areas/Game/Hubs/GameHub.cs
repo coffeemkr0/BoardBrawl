34e416f3b8a71c7de36d26932e355c1fc43886c4
Michael Coffey <mike.e.coffey@gmail.com>
2023-07-09 15:06:07 -0500

    #43 try to make some of the async initialization stuff easier to deal with

--- src/BoardBrawl.WebApp.MVC/Areas/Game/Hubs/GameHub.cs
+++ src/BoardBrawl.WebApp.MVC/Areas/Game/Hubs/GameHub.cs
@@ -5,15 +5,19 @@ namespace BoardBrawl.WebApp.MVC.Areas.Game.Hubs
 {
     public class GameHub : Hub
     {
+        private readonly ILogger<GameHub> _logger;
         private readonly IService _service;
 
-        public GameHub(IService service)
+        public GameHub(ILogger<GameHub> logger, IService service)
         {
+            _logger = logger;
             _service = service;
         }
 
         public async Task JoinGameHub(string gameId, string playerId, Guid peerId)
         {
+            _logger.LogInformation($"Player joined game hub. gameId:{gameId}, playerId:{playerId}, peerId:{peerId}");
+
             _service.UpdatePeerId(Convert.ToInt32(playerId), peerId);
             await Groups.AddToGroupAsync(Context.ConnectionId, gameId.ToString());
             await Clients.GroupExcept(gameId.ToString(), Context.ConnectionId).SendAsync("OnPlayerJoined", playerId, peerId);
